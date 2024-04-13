90b51d13e9158e131dc22a74e44e190fd750de3a
JmBeltranCardona <juanmanuelbeltrancardona@gmail.com>
2024-04-13 16:10:38 -0500

    Upload changes with the creation of the project, architecture configuration and implementation of the log service

---
+++ Infrastructure/Logger/Logic/LoggerService.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Logger.Contract;
+using NLog;
+
+namespace Infrastructure.Logger.Logic
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+
+        public void LogDebug(string message) => _logger.Debug(message);
+
+        public void LogError(string message) => _logger.Error(message);
+
+        public void LogInfo(string message) => _logger.Info(message);
+
+        public void LogWarn(string message) => _logger.Warn(message);
+    }
+}
