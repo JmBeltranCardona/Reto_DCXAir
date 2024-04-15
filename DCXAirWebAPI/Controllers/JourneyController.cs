c62301953b99948e8d54ce459a496ae85668644f
JmBeltranCardona <juanmanuelbeltrancardona@gmail.com>
2024-04-14 21:38:33 -0500

    I create the controllers by implementing a main controller with a mediator to promote indirect communication between these components through a centralized object, from there I make the calls to the other controllers

---
+++ DCXAirWebAPI/Controllers/JourneyController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Cqrs.Journey.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DCXAirWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JourneyController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetRoute([FromQuery] GetRouteQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
